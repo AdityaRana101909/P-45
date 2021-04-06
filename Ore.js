9a86009a29453af2aaccb2ef3cfc9a991efd01dd
AdityaRana101909 <71439616+AdityaRana101909@users.noreply.github.com>
2021-04-06 20:48:06 +0530

    Add files via upload

---
+++ Ore.js
@@ -0,0 +1,27 @@
+class Ore{
+  constructor(x,y,width,height) {
+    var options = {
+        isStatic: true
+    }
+    this.body = Bodies.rectangle(x,y,width,height,options);
+    this.width = width;
+    this.height = height;
+    World.add(world, this.body);
+  }
+  /*display() {
+    //this.body.position.x = mouseX;
+    //this.body.position.y = mouseY;
+
+    display();
+
+    if(this.body.velocity.x > 10 && this.body.position.x > 200){
+      var position = [this.body.position.x, this.body.position.y];
+      this.trajectory.push(position);
+    }
+   
+
+    for(var i=0; i<this.trajectory.length; i++){
+      image(this.smokeImage, this.trajectory[i][0], this.trajectory[i][1]);
+    }
+  }*/
+}
